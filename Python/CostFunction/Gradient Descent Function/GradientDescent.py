6ef0e9a5f7633884231ea59d9d623a9332e49b5e
PolopTech <73751987+PolopTechnology@users.noreply.github.com>
2021-02-27 13:50:04 -0500

    sim
    Gradient descent algorithms actually update simultaneously and I forgot. But I updated the py file.

--- Python/CostFunction/Gradient Descent Function/GradientDescent.py
+++ Python/CostFunction/Gradient Descent Function/GradientDescent.py
@@ -4,18 +4,14 @@ def cost_function(location, data_points):
     step2 = location
     while True:
         step1 -= 1
-        for x in data_points:
-            if x == step1:
-                print(str(location) + " is closest to " + str(x))
-                broken = True
         step2 += 1
-        for y in data_points:
-            if y == step2:
-                print(str(location) + " is closest to " + str(y))
+        for x in data_points:
+            if x == step1 or x == step2:
+                print(str(location) + " is closest to " + str(x))
                 broken = True
         if broken == True:
             break
 
-loc = 4
+loc = 5
 data = [1, 3, 7, 14, 23]
 cost_function(loc, data)
